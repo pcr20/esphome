661d1c90152be6d585d6af7ded56593146b8a17e
Paul Roberts <paul.c.roberts@ntlworld.com>
2023-11-02 14:47:25 +0000

    debug

--- esphome/components/modbus_controller/modbus_controller.cpp
+++ esphome/components/modbus_controller/modbus_controller.cpp
@@ -58,13 +58,13 @@ bool ModbusController::send_next_command_() {
 
 // Queue incoming response
 void ModbusController::on_modbus_data(const std::vector<uint8_t> &data) {
-    vector<uint8_t>::const_iterator it;
+    std::vector<uint8_t>::const_iterator it;
     char hex_chars[50];
     int n=0;
     for (it = data.begin(); it != data.end(); it++) {
-        snprintf(hex_chars+n,3 "02%x ", *it);
+        snprintf(hex_chars+n,3, "02%x ", *it);
         n=n+3;
-        if n>=50 break;
+        if (n>=50) break;
     }
     hex_chars[50]='\0';
     ESP_LOGV(TAG, "data: %s", hex_chars);
