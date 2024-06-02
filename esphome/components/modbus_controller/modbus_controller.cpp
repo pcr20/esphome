f570345bdf99393feef7f231131efdb9e7320c37
Paul Roberts <dr.paul.roberts@gmail.com>
2024-06-02 23:09:21 +0200

    test version

--- esphome/components/modbus_controller/modbus_controller.cpp
+++ esphome/components/modbus_controller/modbus_controller.cpp
@@ -73,14 +73,10 @@ void ModbusController::on_modbus_data(bool is_response,uint8_t address,uint8_t f
         ESP_LOGI(TAG, "**Fn: 0x%X A:0x%X #:%d S A:0x%x #:%d off:%d  :%x", function_code,start_address,number_of_registers,sensor->start_address,sensor->register_count,start_offset,
         sensor->glo_registers_); 
 
-            /*for (int i=0;i<number_of_registers;i++)
+            for (int i=0;i<number_of_registers;i++)
               {
                 (*sensor->glo_registers_)[i+start_offset]=((uint16_t)data[2*i+1]) | (((uint16_t)data[2*i]) << 8);
-              }*/
-              (*sensor->glo_registers_)[0]=(uint16_t)(1);
-              (*sensor->glo_registers_)[1]=(uint16_t)(2);
-              (*sensor->glo_registers_)[2]=staticcounter;
-              staticcounter++;
+              }
 
           }
     }
