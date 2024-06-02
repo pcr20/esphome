de619837f8b3121b02e54e63b404fb289b3fa62c
Paul Roberts <dr.paul.roberts@gmail.com>
2024-06-02 20:48:43 +0200

    debug

--- esphome/components/modbus_controller/modbus_controller.cpp
+++ esphome/components/modbus_controller/modbus_controller.cpp
@@ -70,10 +70,13 @@ void ModbusController::on_modbus_data(bool is_response,uint8_t address,uint8_t f
         int start_offset = start_address-sensor->start_address;
         if ((start_address >= sensor->start_address) &&  ((start_address+number_of_registers) <= (sensor->start_address+sensor->register_count))) {
         ESP_LOGI(TAG, "**Fn: 0x%X A:0x%X #:%d S A:0x%x #:%d off:%d", function_code,start_address,number_of_registers,sensor->start_address,sensor->register_count,start_offset);        
-            for (int i=0;i<number_of_registers;i++)
+            /*for (int i=0;i<number_of_registers;i++)
               {
-                (sensor->glo_registers_)[i+start_offset]=uint16_t(data[2*i+1]) | (uint16_t(data[2*i]) << 8);
-              }
+                (*sensor->glo_registers_)[i+start_offset]=uint16_t(data[2*i+1]) | (uint16_t(data[2*i]) << 8);
+              }*/
+              (*sensor->glo_registers_)[0]=uint16_t(1);
+              (*sensor->glo_registers_)[1]=uint16_t(2);
+
           }
     }
     
