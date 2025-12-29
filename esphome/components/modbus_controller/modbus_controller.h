2a6249984af9520e496914e50cb00fa8f69326c7
Paul Roberts <dr.paul.roberts@gmail.com>
2025-12-29 15:23:16 +0000

    bug fix

--- esphome/components/modbus_controller/modbus_controller.h
+++ esphome/components/modbus_controller/modbus_controller.h
@@ -224,6 +224,12 @@ class SensorItem {
   uint32_t bitmask{0};
   uint8_t offset{0};
   uint8_t register_count{0};
+  uint8_t address_in;
+  bool is_response_in;
+  uint8_t function_code_in;
+  uint16_t start_reg_in;
+  uint16_t num_reg_in;
+  uint16_t crc_in;
   uint8_t response_bytes{0};
   uint16_t skip_updates{0};
   std::vector<uint8_t> custom_data{};
@@ -476,14 +482,14 @@ class ModbusController : public PollingComponent, public modbus::ModbusDevice {
   void add_server_register(ServerRegister *server_register) { server_registers_.push_back(server_register); }
   /// called when a modbus response was parsed without errors
   void on_modbus_data(const std::vector<uint8_t> &data) override;
-  void on_modbus_data(bool is_response,uint8_t address,uint8_t function_code, uint16_t start_address,uint16_t number_of_registers,uint16_t crc,const std::vector<uint8_t> &data) override;
+  void on_modbus_data(bool is_response,uint8_t address,uint8_t function_code, uint16_t start_address,uint16_t number_of_registers,uint16_t crc,const std::vector<uint8_t> &data);
   /// called when a modbus error response was received
   void on_modbus_error(uint8_t function_code, uint8_t exception_code) override;
   /// called when a modbus request (function code 0x03 or 0x04) was parsed without errors
   void on_modbus_read_registers(uint8_t function_code, uint16_t start_address, uint16_t number_of_registers) final;
   /// called when a modbus request (function code 0x06 or 0x10) was parsed without errors
   void on_modbus_write_registers(uint8_t function_code, const std::vector<uint8_t> &data) final;
-  void on_modbus_write_registers(uint8_t function_code, uint16_t start_address,uint16_t number_of_registers,const std::vector<uint8_t> &data) final;
+  void on_modbus_write_registers(uint8_t function_code, uint16_t start_address,uint16_t number_of_registers,const std::vector<uint8_t> &data);
   /// default delegate called by process_modbus_data when a response has retrieved from the incoming queue
   void on_register_data(ModbusRegisterType register_type, uint16_t start_address, const std::vector<uint8_t> &data);
   /// default delegate called by process_modbus_data when a response for a write response has retrieved from the
