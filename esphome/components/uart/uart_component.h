f24bcaea287a522557b15e2b24116b805820ce70
Paul Roberts <dr.paul.roberts@gmail.com>
2026-04-13 18:21:34 +0100

    tx buffer bugfix

--- esphome/components/uart/uart_component.h
+++ esphome/components/uart/uart_component.h
@@ -211,6 +211,7 @@ class UARTComponent {
   InternalGPIOPin *rx_pin_{};
   InternalGPIOPin *flow_control_pin_{};
   size_t rx_buffer_size_{};
+  size_t tx_buffer_size_;
   // ESP32 (both Arduino and ESP-IDF) always sets this at codegen time via set_rx_full_threshold().
   // Other platforms (USB UART, Arduino, etc.) leave it unset.
   size_t rx_full_threshold_{RX_FULL_THRESHOLD_UNSET};
